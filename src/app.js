7bde05857980f75a9fe60f5d9991de8b15f2831e
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2024-12-12 22:48:02 +0700

    init package morgan, helmet, compression

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 // ES modules (ES6+)
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
 
 const app = express();
 
+
 // Init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
 
 // Init database
 
 // Init routes
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello Fantipjs';
+    return res.status(200).json({
+        message: 'Welcome Fantipjs',
+        metadata: strCompress.repeat(100000)
+    });
+});
 
 // Handle errors
 
